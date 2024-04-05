1ee1e6721635568c272e42f8d243b297c927b1a1
Mikhailo <71217534+Supchik22@users.noreply.github.com>
2024-04-05 10:41:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-#=========Import Modules=========================================---.
+# Import Modules
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tkinter import *
 import customtkinter
 from customtkinter import *
 import sys
-#============================Theme================================
+
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-#=================================================================
+
 def open_file():
     #Open a file for editing.
     filepath = askopenfilename(
@@ -21,7 +21,7 @@ def open_file():
         text = input_file.read()
         txt_edit.insert(customtkinter.END, text)
     window.title(f"Example notebook - {filepath}")
-#=================================================================---.
+
 def save_file(event):
     #Save a file.
     filepath = asksaveasfilename(
@@ -34,37 +34,36 @@ def save_file(event):
         text = txt_edit.get(1.0, tk.END)
         output_file.write(text)
     window.title(f"Example notebook - {filepath}")
-#=================================================================|
-def s_file():#							  |
-	save_file("e")#						  |
-#=================================================================|
-def onKeyPress(event):#						  |
-    print("ctrl+a")#						  |
-#=================================================================|
-window = customtkinter.CTk()#					  |
-#=====================Window======================================|
-window.title("Example notebook ")#                                       |
-window.rowconfigure(0, minsize=800, weight=1)#                    |
-window.columnconfigure(1, minsize=800, weight=1)#                 |
-#=======================Elements==================================|
+
+def s_file():#							  
+	save_file("e")#						  
+
+def onKeyPress(event):#						  
+    print("ctrl+a")#						  
+
+window = customtkinter.CTk()#					  
+
+window.title("Example notebook ")#                                       
+window.rowconfigure(0, minsize=800, weight=1)#                    
+window.columnconfigure(1, minsize=800, weight=1)#                 
 
 
-txt_edit = tk.Text(window)#                                     =============================|
-fr_buttons = customtkinter.CTkFrame(window, relief=tk.RAISED, bd=2)#                            |
-btn_open = customtkinter.CTkButton(fr_buttons, text="Open", command=open_file) # Open Button    |
+
+txt_edit = tk.Text(window)#                                    
+fr_buttons = customtkinter.CTkFrame(window, relief=tk.RAISED, bd=2)#                   
+btn_open = customtkinter.CTkButton(fr_buttons, text="Open", command=open_file)
 thisMenuBar = Menu(window)
 
-btn_save = customtkinter.CTkButton(fr_buttons, text="Save As...", command=s_file) # Save Button |
-#===============================================================================================|
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5) #     |
-btn_save.grid(row=1, column=0, sticky="ew", padx=5) #             |
-#=================================================================|
-fr_buttons.grid(row=0, column=0, sticky="ns")#                    |
-txt_edit.grid(row=0, column=1, sticky="nsew")#                    |
-window.bind('<Control-a>', onKeyPress)#                           |
-window.bind('<Control-s>', save_file)#                            |
-scrollb = tk.Scrollbar(window, command=txt_edit.yview)#           |
-#scrollb.grid(row=0, column=0)#, sticky='nsew')                   |
-#============View Window==========================================|
-window.mainloop()#                                                |
-#=================================================================|
+btn_save = customtkinter.CTkButton(fr_buttons, text="Save As...", command=s_file) 
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)    
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)             
+
+fr_buttons.grid(row=0, column=0, sticky="ns")#                    
+txt_edit.grid(row=0, column=1, sticky="nsew")#                    
+window.bind('<Control-a>', onKeyPress)#                           
+window.bind('<Control-s>', save_file)#                           
+scrollb = tk.Scrollbar(window, command=txt_edit.yview)#           
+#scrollb.grid(row=0, column=0)#, sticky='nsew')                   
+
+window.mainloop()#                                                
